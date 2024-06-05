b1ad0a40e1161e2e4f1391d1da29f16a17a92b03
rgaibor17 <108085022+rgaibor17@users.noreply.github.com>
2024-06-05 14:43:13 -0500

    Class 08

--- js/main.js
+++ js/main.js
@@ -17,15 +17,53 @@ window.addEventListener("DOMContentLoaded", loaded);*/
 
 window.addEventListener("DOMContentLoaded", loaded);*/
 
-let loaded = ( eventLoaded ) => {
+let loaded = (eventLoaded) => {
 
-    let myform = document.getElementById('form');
-    
-    myform.addEventListener('submit', ( eventSubmit ) => { 
-  
-      debugger;
-  
-    })
-  
-  }
+  let myform = document.getElementById('form');
+
+  myform.addEventListener('submit', (eventSubmit) => {
+
+    debugger;
+
+  })
+
+}
 window.addEventListener("DOMContentLoaded", loaded);
+
+form.addEventListener('submit', (eventSubmit) => {
+
+  eventSubmit.preventDefault()
+  if (form_name.value.length == 0) {
+    alert("Nombre requerido")
+    form_name.focus()
+    return;
+  }
+  if (form_email.value.length == 0) {
+    alert("Email requerido")
+    form_email.focus()
+    return;
+  }
+})
+
+const formulario = document.getElementById('form');
+formulario.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const nombre = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const datos = {
+    nombre: nombre,
+    email: email,
+  };
+  fetch('https://console.firebase.google.com/project/dawm-2925a/database/dawm-2925a-default-rtdb/data/~2F?hl=es-419', {
+    method: 'POST',
+    body: JSON.stringify(datos),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(respuesta => respuesta.json())
+    .then(datos => {
+      console.log(datos); // Imprimir la respuesta del servidor
+    })
+    .catch(error => console.error(error));
+  });
