39a77c3932344c892a64106d4fec310f34682937
rgaibor17 <108085022+rgaibor17@users.noreply.github.com>
2024-06-12 14:46:27 -0500

    Class 10

--- js/results.js
+++ js/results.js
@@ -1,39 +1,37 @@
+let loaded = ( eventLoaded ) => {
+    async function obtenerDatos() {
+        const url = "https://dawm-2925a-default-rtdb.firebaseio.com/Collection.json"; // Reemplaza con la URL real de la API o recurso
+        const respuesta = await fetch(url);
+        if (!respuesta.ok) {
+        console.error("Error:", respuesta.status);
+        return;
+        }
+        const datos = await respuesta.json();
+        let map = new Map();
 
+        let total = 0;
+        let tablebody = document.getElementById('tablebody');
+        for (const key in datos) {
+            let element = datos[key]
+            let name = element.nombre
+            let counter = map.has(name)?map.get(name)+1:1;
+            map.set(name, counter)
+        }
 
-    let loaded = ( eventLoaded ) => {
-        async function obtenerDatos() {
-            const url = "https://dawm-2925a-default-rtdb.firebaseio.com/Collection.json"; // Reemplaza con la URL real de la API o recurso
-            const respuesta = await fetch(url);
-            if (!respuesta.ok) {
-            console.error("Error:", respuesta.status);
-            return;
-            }
-            const datos = await respuesta.json();
-            let map = new Map();
+        for (let key of map.keys()) {
+            let template = `
+            <tr>
+                <td>${key}</td>
+                <td>${map.get(key)}</td>
+            </tr>
+            `
+            total += map.get(key);
+            tablebody.innerHTML += template
+        }
 
-            let total = 0;
-            let tablebody = document.getElementById('tablebody');
-            for (const key of datos) {
-                let element = datos[key]
-                let name = element['name']
-                let counter = map.has(name)?map.get(name)+1:1;
-                map.set(name, counter)
-            }
+        console.log(datos); // Procesar o mostrar los datos obtenidos
+        }
+        obtenerDatos();
+}
 
-            for (let key of map.keys()) {
-                let template = `
-                <tr>
-                    <td>${key}</td>
-                    <td>${map.get(key)}</td>
-                </tr>
-                `
-                total += map.get(keys);
-                tablebody.innerHTML += template
-            }
-
-            console.log(datos); // Procesar o mostrar los datos obtenidos
-            }
-            obtenerDatos();
-    }
-    
-    window.addEventListener("DOMContentLoaded", loaded);
+window.addEventListener("DOMContentLoaded", loaded);
