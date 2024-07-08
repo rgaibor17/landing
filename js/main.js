edf39a2cbbada43d5ae8dc42f301c392d32fb738
rgaibor17 <rgaibor@espol.edu.ec>
2024-07-07 22:25:18 -0500

    Cambios a secciones restantes. Cambios adicionales. Assets

--- js/main.js
+++ js/main.js
@@ -1,3 +1,53 @@
+/* Revisar la válidez de los valores recibidos */
+
+function checkData() {
+  if (form_name.value.length == 0) {
+    alert("Nombre requerido")
+    form_name.focus()
+    return;
+  }
+  if (form_email.value.length == 0) {
+    alert("Email requerido")
+    form_email.focus()
+    return;
+  }
+}
+
+/* Procesar y guardar los datos obtenidos del formulario */
+
+const formulario = document.getElementById('form');
+formulario.addEventListener('submit', (event) => {
+  event.preventDefault();
+  checkData();
+  const nombre = document.getElementById('form_name').value
+  const email = document.getElementById('form_email').value
+  const datos = {
+    nombre: nombre,
+    email: email,
+  };
+  fetch('https://dawm-2925a-default-rtdb.firebaseio.com/Collection.json', {
+    method: 'POST',
+    body: JSON.stringify(datos),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(respuesta => respuesta.json())
+    .then(datos => {
+      console.log(datos); // Imprimir la respuesta del servidor
+    })
+    .catch(error => console.error(error));
+    window.alert("Respuesta guardada");
+  });
+
+/* Vaciar el contenido del formulario al recargar la página */
+
+window.addEventListener("DOMContentLoaded", loaded);
+let loaded = ( eventLoaded ) => {
+  window.alert("landing page loaded");
+  formulario.getElementById("form").reset();
+}
+
 /*let loaded = ( eventLoaded ) => {
   
     window.alert("landing page loaded");
@@ -28,43 +78,5 @@ window.addEventListener("DOMContentLoaded", loaded);*/
   })
 
 }
+
 window.addEventListener("DOMContentLoaded", loaded);*/
-
-form.addEventListener('submit', (eventSubmit) => {
-
-  eventSubmit.preventDefault()
-  if (form_name.value.length == 0) {
-    alert("Nombre requerido")
-    form_name.focus()
-    return;
-  }
-  if (form_email.value.length == 0) {
-    alert("Email requerido")
-    form_email.focus()
-    return;
-  }
-})
-
-const formulario = document.getElementById('form');
-formulario.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const nombre = document.getElementById('form_name').value
-  const email = document.getElementById('form_email').value
-  const datos = {
-    nombre: nombre,
-    email: email,
-  };
-  fetch('https://dawm-2925a-default-rtdb.firebaseio.com/Collection.json', {
-    method: 'POST',
-    body: JSON.stringify(datos),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-    .then(respuesta => respuesta.json())
-    .then(datos => {
-      console.log(datos); // Imprimir la respuesta del servidor
-    })
-    .catch(error => console.error(error));
-    window.alert("Respuesta guardada");
-  });
